e59b8ace6808b9e0916a0222b298342ff044a13c
Maksim Manylow <maksimmanylow@gmail.com>
2018-07-31 01:51:08 +0300

    made StarRating presentational cpmponent

--- src/client/components/StarRating.jsx
+++ src/client/components/StarRating.jsx
@@ -2,38 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Star from './Star';
 
-class StarRating extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      starsSelected: props.starsSelected || 0,
-    };
-    this.change = this.change.bind(this);
-  }
-
-  change(starsSelected) {
-    this.setState({ starsSelected });
-  }
-
-  render() {
-    const { totalStars } = this.props;
-    const { starsSelected } = this.state;
-
-    return (
-      <div className="star-rating">
-        {[...Array(totalStars)].map((n, i) => (
-          <Star
-            key={i}
-            selected={i < starsSelected}
-            onClick={() => this.change(i + 1)}
-          />
-        ))}
-        <p>
-          {starsSelected} of {totalStars} stars
-        </p>
-      </div>
-    );
-  }
-}
+const StarRating = ({ starsSelected = 0, totalStars = 5, onRate = f => f }) => (
+  <div className="star-rating">
+    {[...Array(totalStars)].map((n, i) => (
+      <Star
+        key={i}
+        selected={i < starsSelected}
+        onClick={() => this.change(i + 1)}
+      />
+    ))}
+    <p>
+      {starsSelected} of {totalStars} stars
+    </p>
+  </div>
+);
 
 export default StarRating;
